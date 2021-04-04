258a9ef1ac70943d333afe892fb3fb54af7326f3
Krzem <krzem5.dev@gmail.com>
2021-04-04 10:53:54 +0200

    Push Update 04/04/2021, 10:53:39

--- src/rsa/rsa.c
+++ src/rsa/rsa.c
@@ -1,15 +1,16 @@
 #include <rsa.h>
 #include <windows.h>
 #include <wincrypt.h>
-#include <intrin.h>
 #include <stdint.h>
 #include <stdio.h>
-#pragma intrinsic(__stosq)
 
 
 
+#define MILLER_RABIN_LOOP_LENGTH 40
 
-uint64_t _mul_mod(uint64_t a,uint64_t b,uint64_t m){
+
+
+uint64_t _mult_mod(uint64_t a,uint64_t b,uint64_t m){
 	uint64_t o=0;
 	while (a){
 		if (a&1){
@@ -23,51 +24,104 @@ uint64_t _mul_mod(uint64_t a,uint64_t b,uint64_t m){
 
 
 
+uint64_t _pow_mod(uint64_t pb,uint64_t t,uint64_t m){
+	uint64_t o=1;
+	while (t){
+		if (t&1){
+			o=_mult_mod(o,pb,m);
+		}
+		pb=_mult_mod(pb,pb,m);
+		t>>=1;
+	}
+	return o;
+}
+
+
+
+uint8_t _check_prime(uint64_t n,HCRYPTPROV ctx){
+	const static uint64_t LOW_PRIMES[]={3,5,7,11,13,17,19,23,29,31,37,41,43,47,53,59,61,67,71,73,79,83,89,97,101,103,107,109,113,127,131,137,139,149,151,157,163,167,173,179,181,191,193,197,199,211,223,227,229,233,239,241,251,257,263,269,271,277,281,283,293,307,311,313,317,331,337,347,349,353,359,367,373,379,383,389,397,401,409,419,421,431,433,439,443,449,457,461,463,467,479,487,491,499,503,509,521,523,541,547,557,563,569,571,577,587,593,599,601,607,613,617,619,631,641,643,647,653,659,661,673,677,683,691,701,709,719,727,733,739,743,751,757,761,769,773,787,797,809,811,821,823,827,829,839,853,857,859,863,877,881,883,887,907,911,919,929,937,941,947,953,967,971,977,983,991,997};
+	if (_pow_mod(2,n-1,n)!=1){
+		return 0;
+	}
+	for (uint16_t i=0;i<sizeof(LOW_PRIMES)/sizeof(uint64_t);i++){
+		if (n==LOW_PRIMES[i]){
+			return 1;
+		}
+		if (!(n%LOW_PRIMES[i])){
+			return 0;
+		}
+	}
+	uint64_t s=n-1;
+	uint64_t t=0;
+	while (!(s&1)){
+		s>>=1;
+		t++;
+	}
+	uint8_t i=0;
+	for (;i<64;i++){
+		if (!(n>>i)){
+			break;
+		}
+	}
+	uint8_t j=(i+7)>>3;
+	i=(uint8_t)((1<<(uint16_t)(i&7?i&7:8))-1);
+	uint64_t a;
+	BYTE* bf=(BYTE*)&a;
+	for (uint8_t k=0;k<MILLER_RABIN_LOOP_LENGTH;k++){
+		a=0;
+		while (a<2||a>=n){
+			CryptGenRandom(ctx,j,bf);
+			bf[j-1]&=i;
+		}
+		uint64_t v=_pow_mod(a,s,n);
+		if (v!=1){
+			uint8_t l=0;
+			while (v!=n-1){
+				if (l==t-1){
+					return 0;
+				}
+				l++;
+				v=_mult_mod(v,v,n);
+			}
+		}
+	}
+	return 1;
+}
+
+
+
 void rsa_create_keypair(rsa_keypair_t* kp){
 	HCRYPTPROV ctx;
 	CryptAcquireContext(&ctx,NULL,NULL,PROV_RSA_FULL,CRYPT_VERIFYCONTEXT);
 	uint64_t p=0;
 _gen_p:
 	CryptGenRandom(ctx,sizeof(uint64_t),(BYTE*)&p);
-	// uint64_t t=p-1;
-	// uint64_t c=1;
-	// while (t){
-	// 	uint64_t s=(t>32?32:t);
-	// 	t-=s;
-	// 	c=(c<<s)%p;
-	// }
 	p&=~0x8000000000000000;
+	p|=1;
 	if (p<100){
 		goto _gen_p;
 	}
-	uint64_t pb=2;
-	uint64_t pt=p-1;
-	uint64_t o=1;
-	while (pt){
-		if (pt&1){
-			o=_mul_mod(o,pb,p);
-		}
-		pb=_mul_mod(pb,pb,p);
-		pt>>=1;
-	}
-	if (o!=1){
-		printf("%llu\n",p);
+	if (!_check_prime(p,ctx)){
 		goto _gen_p;
 	}
 	uint64_t q=0;
-	while (q<100){
-		CryptGenRandom(ctx,sizeof(uint64_t),(BYTE*)&q);
+_gen_q:
+	CryptGenRandom(ctx,sizeof(uint64_t),(BYTE*)&q);
+	q&=~0x8000000000000000;
+	q|=1;
+	if (q<100){
+		goto _gen_q;
 	}
-	printf("%llu, %llu\n",p,q);
-	/***/p=61;q=53;/***/
+	if (!_check_prime(q,ctx)){
+		goto _gen_q;
+	}
 	kp->n=p*q;
 	p--;
 	q--;
 	uint64_t a=p;
 	uint64_t b=q;
-	uint64_t t;
 	while (b){
-		t=b;
+		uint64_t t=b;
 		b=a%b;
 		a=t;
 	}
@@ -81,7 +135,7 @@ _gen_p:
 	kp->e=0;
 	BYTE* bf=(BYTE*)&kp->e;
 	uint8_t j=(i+7)>>3;
-	i&=7;
+	i=(uint8_t)((1<<(uint16_t)(i&7?i&7:8))-1);
 	while (1){
 		CryptGenRandom(ctx,j,bf);
 		bf[j-1]&=i;
@@ -91,7 +145,7 @@ _gen_p:
 		a=kp->e;
 		b=lcm;
 		while (b){
-			t=b;
+			uint64_t t=b;
 			b=a%b;
 			a=t;
 		}
